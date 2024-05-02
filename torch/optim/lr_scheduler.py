a9b86c731c75e94ca54c42c29dca7c4ac3d8f0a6
Michael Lazos <mlazos@meta.com>
2024-05-02 01:00:15 -0700

    Enable LR Scheduler to update tensor LR
    ghstack-source-id: 1382df444d42eaab354a0de761c351c3df6e8a68
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123753

--- torch/optim/lr_scheduler.py
+++ torch/optim/lr_scheduler.py
@@ -7,7 +7,7 @@ from functools import partial
 from typing import Optional, Sequence
 from weakref import ref
 
-from torch import inf
+from torch import inf, Tensor
 
 from .optimizer import Optimizer
 
@@ -182,7 +182,10 @@ class LRScheduler:
 
         for i, data in enumerate(zip(self.optimizer.param_groups, values)):
             param_group, lr = data
-            param_group["lr"] = lr
+            if isinstance(param_group["lr"], Tensor):
+                param_group["lr"].fill_(lr)
+            else:
+                param_group["lr"] = lr
 
         self._last_lr = [group["lr"] for group in self.optimizer.param_groups]
 
@@ -1447,7 +1450,10 @@ class CyclicLR(LRScheduler):
         base_lrs = self._format_param("base_lr", optimizer, base_lr)
         if last_epoch == -1:
             for lr, group in zip(base_lrs, optimizer.param_groups):
-                group["lr"] = lr
+                if isinstance(group["lr"], Tensor):
+                    group["lr"].fill_(lr)
+                else:
+                    group["lr"] = lr
 
         self.max_lrs = self._format_param("max_lr", optimizer, max_lr)
 
